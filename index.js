83d95b1896f6b88819683d945e4fd83181b25777
Amia Defreitas <amia.defreitas0@gmail.com>
2019-08-30 19:08:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-'use strict';
-
-	function theBeatlesPlay(players, instruments) {
-	    var allPlayers = [];
-	    for (var i=0; i < players.length; i++) {
-	        allPlayers.push(players[i] + " plays " + instruments[i]);
-	    }
-	    return allPlayers;
+function theBeatlesPlay(musicians, instruments) {
+	  var array = [];
+	  for (var i = 0; i < musicians.length; i++) {
+	  	array.push(musicians[i] + ' plays ' + instruments[i]);
+	  }
+	  return array;
 	}
 
-	function johnLennonFacts() {
-	    var facts = [];
-	    var newFacts = [];
-	    var i = 0;
-	    while (i < facts.length) {
-	        newFacts.push(facts[i] +"!!!");
-	        i++;
-	    }
-	    return newFacts;
+	function johnLennonFacts(facts) {
+	  var results = [];
+	  var count = 0;
+	  while (count < facts.length) {
+	    results.push(facts[count] + "!!!");
+	    count++;
+	  }
+	  return results;
 	}
 
 	function iLoveTheBeatles(num) {
-	    var love = [];
-	    do {
-	        love.push("I love the Beatles!");
-	        num++;
-	    } while (num < 15);
-
-	    return love;
+	  var array = [];
+	  do {
+	    array.push("I love the Beatles!")
+	    num++;
+	  } while (num < 15);
+	  return array;
 	}
