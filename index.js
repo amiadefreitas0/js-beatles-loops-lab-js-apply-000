39bed049862f62b162be25e51378ef9b4dc0e452
Amia Defreitas <amia.defreitas0@gmail.com>
2019-08-30 19:04:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 'use strict';
 function theBeatlesPlay(players, instruments){
   var allPlayers =[];
-  for (var i=0;i < players.length;i++){
+  for (var i=0;i < players.length; i++){
     allPlayers.push( players[i]  + " plays " + instruments[i]);
   }
 return allPlayers;
