f8cc93e42a24527291192bf0c6c1f997243f685c
Amia Defreitas <amia.defreitas0@gmail.com>
2019-08-30 19:04:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-// add solution here
 'use strict';
-function theBeatlesPlay(players, instruments){
-  var allPlayers =[];
-  for (var i=0;i < players.length; i++){
-    allPlayers.push( players[i]  + " plays " + instruments[i]);
-  }
-return allPlayers;
-}
 
-function johnLennonFacts() {
+	function theBeatlesPlay(players, instruments) {
+	    var allPlayers = [];
+	    for (var i=0; i < players.length; i++) {
+	        allPlayers.push(players[i] + " plays " + instruments[i]);
+	    }
+	    return allPlayers;
+	}
+
+	function johnLennonFacts() {
 	    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
 	    var newFacts = [];
-	    var i=0;
+	    var i = 0;
 	    while (i < facts.length) {
 	        newFacts.push(facts[i] +"!!!");
 	        i++;
 	    }
-return newFacts;
-}
+	    return newFacts;
+	}
 
-function iLoveTheBeatles(num){
-  var love=[];
-  do{
-    love.push("I love the Beatles!");
-    num++;
+	function iLoveTheBeatles(num) {
+	    var love = [];
+	    do {
+	        love.push("I love the Beatles!");
+	        num++;
+	    } while (num < 15);
 
-  } while (num < 15);
-  return love;
-}
+	    return love;
+	}
